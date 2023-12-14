bf9048c84452851d55b81fb2ad2e083924bd348b
vivianmartinez <128734243+vivianmartinez@users.noreply.github.com>
2023-12-14 17:50:15 +0100

    modify method create book in controller

--- api/BookController.php
+++ api/BookController.php
@@ -48,7 +48,33 @@ class BookController{
         $jsonData = file_get_contents('php://input');
         $data = json_decode($jsonData,true);
         if($data !== null){
-            print_r($data);
+            if(array_key_exists('title',$data)){
+                $this->book->setTitle(htmlspecialchars(strip_tags($data['title'])));
+            }else{
+                $this->jsonResponse(500,'Title cannot be null.');
+            }
+            if(array_key_exists('description',$data)){
+                $this->book->setDescription(htmlspecialchars(strip_tags($data['description'])));
+            }
+            if(array_key_exists('author_id',$data)){
+                $this->book->setAuthorId(htmlspecialchars(strip_tags($data['author_id'])));
+            }else{
+                $this->jsonResponse(500,'author id cannot be null.');
+            }
+            if(array_key_exists('category_id',$data)){
+                $this->book->setCategoryId(htmlspecialchars(strip_tags($data['category_id'])));
+            }else{
+                $this->jsonResponse(500,'category id cannot be null.');
+            }
+
+            $result = $this->book->create();
+            if($result){
+                $find = $this->book->findOne();
+                $this->jsonResponse(200,$find);
+            }else{
+                $this->jsonResponse(500,'Something bad happened. Can\'t create new book.');
+            }
+            
         }else{
             $this->jsonResponse(404,'You must send a data json.');
         }
