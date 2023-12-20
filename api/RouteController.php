7805f15c7f142518f2550204901201c01aea720f
vivianmartinez <128734243+vivianmartinez@users.noreply.github.com>
2023-12-20 14:13:40 +0100

    add comments to route controller

--- api/RouteController.php
+++ api/RouteController.php
@@ -4,15 +4,14 @@ class RouteController{
     static public function route(){
         include_once 'routes/routes.php';
     }
+    //return controller 
     static public function selectController($routes)
     {
         $controller = null;
-        //print_r($routes[1]); exit();
         if(str_contains($routes[1],'book?') || $routes[1] == 'books'){
                 $controller = 'BookController';
         }elseif(str_contains($routes[1],'author?') || $routes[1] == 'authors'){
                 $controller = 'AuthorController';
-            
         }elseif(str_contains($routes[1],'category?') || $routes[1] == 'categories'){
                 $controller = 'CategoryController';
         }
