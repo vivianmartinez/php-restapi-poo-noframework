72364e25c35364ffa358036b424f495597300362
vivianmartinez <128734243+vivianmartinez@users.noreply.github.com>
2023-12-24 18:45:48 +0100

    modify route controller

--- api/RouteController.php
+++ api/RouteController.php
@@ -8,11 +8,11 @@ class RouteController{
     static public function selectController($routes)
     {
         $controller = null;
-        if(str_contains($routes[1],'book?') || $routes[1] == 'books'){
+        if(str_contains($routes[1],'book?') || $routes[1] == 'book' || $routes[1] == 'books'){
                 $controller = 'BookController';
-        }elseif(str_contains($routes[1],'author?') || $routes[1] == 'authors'){
+        }elseif(str_contains($routes[1],'author?') || $routes[1] == 'author' || $routes[1] == 'authors'){
                 $controller = 'AuthorController';
-        }elseif(str_contains($routes[1],'category?') || $routes[1] == 'categories'){
+        }elseif(str_contains($routes[1],'category?') || $routes[1] == 'category' || $routes[1] == 'categories'){
                 $controller = 'CategoryController';
         }
         return $controller;
